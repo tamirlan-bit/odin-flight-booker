8c6e5751ca5e81c03d03911c5cd97ef765cefab8
tamirlan-bit <ktimor@gmail.com>
2026-03-06 22:38:45 -0800

    base is done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,8 +3,38 @@ class BookingsController < ApplicationController
     if params[:flight_id].present?
       @flight = Flight.find(params[:flight_id])
       @booking = Booking.new(flight: @flight)
+
+      count = params[:passengers_count].to_i
+      count = 1 if count < 1
+
+      count.times do
+        @booking.passengers.build
+      end
+
+      @passengers_count = count
     else
       redirect_to root_path, notice: "Please select a flight first."
     end
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name])
+  end
+  
 end
